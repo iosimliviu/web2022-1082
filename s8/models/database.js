35b8acb8b68b520d392b7b7334141da935d82940
liviuiosim <liviuiosim@gmail.com>
2022-11-23 07:51:28 +0200

    updated s8

--- s8/models/database.js
+++ s8/models/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
 	dialect: "sqlite",
-	storage: "../sqlite/test.db"
+	storage: "./sqlite/test.db"
 })
 
 sequelize.sync({
