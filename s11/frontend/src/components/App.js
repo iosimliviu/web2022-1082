b9d1c868265faf6f9c3f565c6137b264b69e19e7
liviuiosim <liviuiosim@gmail.com>
2023-01-09 00:40:47 +0200

    assignment3 & assignment4 & s11

---
+++ s11/frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import EmployeeList from './EmployeeList'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          I am app.
+        </p>
+        <EmployeeList/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
